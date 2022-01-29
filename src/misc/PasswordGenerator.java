1d5946fa7b26dd0e53a9c89179941cc5e9f5f26b
Enrique Coello <enrique.coello@gmail.com>
2022-01-28 22:31:40 -0500

    Updated the git ignore file and created a password generator file

---
+++ src/misc/PasswordGenerator.java
@@ -0,0 +1,32 @@
+package misc;
+
+import java.util.Random;
+
+public class PasswordGenerator {
+    public static void main(String[] args) {
+        PasswordGenerator passwordGenerator = new PasswordGenerator();
+        int passwordLength = 16;
+
+        String lower = "abcdefghijklmnopqrstuvwxyz";
+        String upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String numbers = "0123456789";
+        String symbols = "[]{}()*;/,_-";
+
+        String all = lower + upper + numbers + symbols;
+        System.out.println("All characters: " + all);
+
+        String password = passwordGenerator.myRandom(all, passwordLength);
+        System.out.println("password: " + password);
+    }
+
+    public String myRandom(String input, int length)
+    {
+        StringBuffer stringBuffer = new StringBuffer();
+        Random random = new Random();
+
+        for (int i = 0; i < length; i++)
+            stringBuffer.append(input.charAt(random.nextInt(input.length())));
+
+        return stringBuffer.toString();
+    }
+}
